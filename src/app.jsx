dd8a6793b74045107832d1d2074f57637d7dd1fe
cthom3 <129532871+cthom3@users.noreply.github.com>
2024-11-21 15:36:00 -0700

    creating menu to appear always in the header

--- src/app.jsx
+++ src/app.jsx
@@ -7,3 +7,38 @@ import {AuthState} from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+function App() {
+   const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+   const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+   const [authState, setAuthState] = React.useState(currentAuthState);
+   
+   return (
+    <BrowserRouter>
+        <div className='body bg-secondary text-light'>
+            <header className='container-fluid'>
+                <nav className='navbar fixed-top'>
+                    <div className='navbar-brand'>
+                        Comfort Cooking
+                    </div>
+                    <menu className='navbar-nav'>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to=''>
+                                Login
+                            </NavLink>
+                        </li>
+                        {authState === AuthState.Authenticated && (
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='recipes'>
+                                    Recipes
+                                </NavLink>
+                            </li>
+                        )}
+                        
+                    </menu>
+                </nav>
+            </header>
+        </div>
+    </BrowserRouter>
+   )
+}
+
