1f7542c5a0adb217db947649503901973033ee1c
cthom3 <129532871+cthom3@users.noreply.github.com>
2024-11-21 16:26:14 -0700

    routes and footer added

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import {Login} from './login/login';
 import {Recipes} from '.recipes/recipes';
+import {Recommendations} from '.recommendations/recommendations';
 import {About} from './about/about';
 import {AuthState} from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -33,12 +34,56 @@ function App() {
                                 </NavLink>
                             </li>
                         )}
-                        
+                        {authState === AuthState.Authenticated && (
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='recommendations'>
+                                    Recommendations
+                                </NavLink>
+                            </li>
+                        )}
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='about'>
+                                About
+                            </NavLink>
+                        </li>
                     </menu>
                 </nav>
             </header>
+
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <Login
+                            userName={userName}
+                            authState={authState}
+                            onAuthChange={(userName, authState) => {
+                                setAuthState(authState);
+                                setUserName(userName);
+                            }}
+                        />
+                    }
+                    exact
+                />
+                <Route path='/recipes' element={<Recipes userName={userName} />} />
+                <Route path='/recommendations' element={<Recommendations />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer className='bg-secondary text-light'>
+                <div className='container-fluid'>
+                    <span>Author: Catherine Applegate</span>
+                    <a class="link-light" href="https://github.com/cthom3/Startup">GitHub</a>
+                </div>
+            </footer>
         </div>
     </BrowserRouter>
    )
 }
 
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown,</main>;
+}
+
+export default App;
