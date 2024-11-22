4dee075f38fec0f980173db418a82fd50774d6a2
cthom3 <129532871+cthom3@users.noreply.github.com>
2024-11-21 19:02:51 -0700

    finish edits to about.jsx

---
+++ src/about/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './about.css';
+
+export function About() {
+    const [imageUrl, setImageUrl]=React.useState();
+
+    React.useEffect(() => {
+        setImageUrl('Oatmeal.jpg');
+    })
+
+    return (
+        <main class="container-fluid">
+            <div>
+                <img src={imageUrl} alt='image'/>
+                <p>Comfort Cooking is the site to save recipes so you can find them everytime. You can share and receive recipes from others. You can save notes as well.</p>
+            </div>
+        </main>
+    );
+}
