addb5ace4875ffa6058c2cbd5ea100de797e239f
cthom3 <129532871+cthom3@users.noreply.github.com>
2024-11-29 13:17:44 -0700

    fix unauthenticated page

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -9,15 +9,30 @@ export function Unauthenticated(props){
     const [displayError,setDisplayError]=React.useState(null);
 
     async function loginUser() {
-        localStorage.setItem('userName', userName);
-        props.onLogin(userName);
+        loginOrCreate(`/api/auth/login`);
     }
 
     async function createUser(){
-        localStorage.setItem('UserName', userName);
-        props.onLogin(userName);
+        loginOrCreate(`/api/auth/create`);
     }
     
+    async function loginOrCreate(endpoint){
+        const response = await fetch(endpoint, {
+            method: 'post',
+            body: JSON.stringify({email: userName, password: password}),
+            headers: {
+                'Content-type' : 'application/json; charset=UTF-8',
+            },
+        });
+        if (response?.status === 200){
+            localStorage.setItem('userName', userName);
+            props.onLogin(userName);
+        } else {
+            const body = await response.json();
+            setDisplayError(`Error: ${body.msg}`);
+        }
+    }
+
     return (
         <>
             <div>
