2eef2c7ad2e401b42778fe48835f0a97eee109f4
cthom3 <129532871+cthom3@users.noreply.github.com>
2024-12-11 10:50:03 -0700

    Created ratingNotifier.js

---
+++ src/recipes/ratingNotifier.js
@@ -0,0 +1,61 @@
+const RatingEvent={
+    System:'system',
+    End: 'ratingEnd',
+    Start: 'ratingStart'
+};
+
+class EventMessage {
+    constructor(from, type, value) {
+        this.from=from;
+        this.type=type;
+        this.value=value;
+    }
+}
+
+class RatingEventNotifier {
+    events=[];
+    handlers=[];
+
+    constructor() {
+        let port=window.location.port;
+        const protocol=window.location.protocol;
+        this.socket=new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket.onopen=(event)=>{
+            this.receiveEvent(new EventMessage ('Comfort Cooking', RatingEvent.System, {msg: 'connected'}));
+        };
+        this.socket.onclose=(event)=>{
+            this.receiveEvent(new EventMessage('Comfort Cooking', RatingEvent.System, {msg:'disconnected'}));
+        };
+        this.socket.onmessage=async(msg)=>{
+            try {
+                const event=JSON.parse(await msg.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        };
+    }
+
+    broadcastEvent(from, type, value) {
+        const event = new EventMessage(from, type, value);
+        this.socket.send(JSON.stringify(event));
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler){
+        this.handlers.filter((h)=>h !== handler);
+    }
+
+    receiveEvent(event){
+        this.events.push(event);
+        this.events.forEach((e)=>{
+            this.handlers.forEach((handler)=> {
+                handler(e);
+            });
+        });
+    }
+}
+
+const RatingNotifier=new RatingEventNotifier();
+export {RatingEvent, RatingNotifier};
