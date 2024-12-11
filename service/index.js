06c44bd3c518147b29a690595b3e1a979dcedfd8
cthom3 <129532871+cthom3@users.noreply.github.com>
2024-12-11 10:29:51 -0700

    update index.js

--- service/index.js
+++ service/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB= require('./database.js');
+const {peerProxy}=require('./peerProxy.js');
 
 const authCookieName='token';
 
@@ -102,3 +103,4 @@ const httpService=app.listen(port, ()=> {
     console.log(`Listening on port ${port}`);
 });
 
+peerProxy(httpService);
